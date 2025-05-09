f05d40a9c0fc904dce9ba4909e1ac2747ac0a1de
duckdzung <laiducdung1911@gmail.com>
2025-05-09 23:15:12 +0700

    revert commit

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -66,7 +66,7 @@ export function AppSidebar({ user }: { user: User }) {
               className="flex flex-row gap-3 items-center"
             >
               <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Botion
+                Action Agent
               </span>
             </Link>
           </div>
